d2f79b8e19230d0559cc9de8645ef86a66c0ac16
Brian C. Winkler <brian.c.winkler@gmail.com>
2014-11-13 17:16:51 +0100

    Added skeleton of Functional classes.

---
+++ @EOLSFunctional/EOLSFunctional.m
@@ -0,0 +1,19 @@
+classdef EOLSFunctional < ObjectiveFunctional
+    %EOLSFunctional  Brief class summary
+    % Detailed description of class.
+    %
+    % See also: OTHER_FUNCTION_NAME1,  OTHER_FUNCTION_NAME2
+    %
+    % Author:  Brian C. Winkler
+    % Email:   brian.c.winkler@gmail.com
+    % Created: 2014-11-13
+    properties
+        
+    end
+    methods
+        function obj = EOLSFunctional( s )
+        % Summary of class constructor.
+            obj@ObjectiveFunctional(s);
+        end
+    end
+end
---
+++ @EOLSFunctional/eval.m
@@ -0,0 +1,34 @@
+function varargout = eval( obj, muDsc )
+%eval - Evaluates the EOLS Functional at muDsc
+%
+% Syntax:  [j, g, H] = J( muDsc )
+%
+% Inputs:
+%    muDsc - Discretized mu value
+%
+% Outputs:
+%    J     - Functional value at muDsc
+%    gJ    - Gradient of J at muDsc
+%    hJ    - Hessian of J at muDSC
+%
+%
+% See also: ObjectiveFunctional, Discretization, SolutionMap
+%
+% Author:  Brian C. Winkler
+% Email:   brian.c.winkler@gmail.com
+% Created: 2014-11-13
+%
+    d = obj.discretization;
+
+    varargout{1} = 1;
+
+    if( nargout > 1)
+        % Calculate the gradient.
+        varargout{2} = zeros(d.dDOF,1);
+    end
+
+    if( nargout == 3)
+        % Calculate the Hessian.
+        varargout{3} = zeros(d.dDOF, d.dDOF);
+    end
+end
